8a46c52e3651850d89d7420487284d332aa5b612
qixiong.li <qixiong.li@qiyuesuo.com>
2022-05-08 13:21:11 +0800

    class、custom-iterator

--- Class/class.js
+++ Class/class.js
@@ -1,24 +1,64 @@
 class Person {
+    field = 'abc'; // 公开属性
+    #field = '_abc'; // 私有属性，实例无法访问
+
     constructor(name, age) {
+        console.log(
+            '通过 new.target 可确定构造函数是如何调用的，也可禁止实例化，仅用来继承（相当于abstract关键字）',
+            new.target === Person
+        );
+
         this.name = name;
         this.age = age;
     }
 
-    static mobile = `13127501027`;
-
-    static sleep(baseInfo, time) {
-        console.log(`${baseInfo}：我要睡觉了，当前时间-${time}`);
-    }
-
-    get baseInfo() {
+    // 计算属性
+    get info() {
         return `个人信息：姓名-${this.name}，年龄-${this.age}`;
     }
 
+    // 静态成员、方法
+    static country = `🇨🇳🇨🇳🇨🇳`;
+    static say() {
+        try {
+            console.log('静态方法打印私有属性 ->', this.#field); // Uncaught TypeError: Cannot read private member #field from an object whose class did not declare it
+        } catch (err) {
+            console.log('[ err ]', err);
+        }
+    }
+
     say() {
-        console.log(this.age);
+        console.log('实例方法打印私有属性 ->', this.#field);
     }
 }
 
 var personA = new Person('lqx', 29);
+console.log('[ personA ]', personA);
+console.log('[ personA ]', personA['field']);
+console.log('[ personA ]', personA['#field']);
 
-Person.sleep(personA.baseInfo, Date.now());
+Person.say();
+personA.say();
+
+//
+// --------------------------------------------
+//
+
+/**
+ * ES6 允许继承原生构造函数定义子类，因为 ES6 是先新建父类的实例对象this，然后再用子类的构造函数修饰this，使得父类的所有行为都可以继承。
+ * 下面是一个继承Array的例子。
+ */
+class MyArray extends Array {
+    constructor(...args) {
+        super(...args);
+    }
+
+    log() {
+        console.log(JSON.stringify(this));
+    }
+}
+
+let arr = new MyArray(1, 2, 3);
+arr = arr.concat([4, 5, [6, 7]]);
+arr.push(8);
+arr.log();
---
+++ Iterator/custom-iterator.js
@@ -0,0 +1,68 @@
+/**
+ * 迭代协议
+ *
+ * 参照可迭代协议，要成为可迭代对象首先要有一个 **@@iterator **即（Symbol.iterator）属性，该属性为一个【无参数的函数】，返回一个符合迭代器协议的对象。
+ * 根据迭代器协议定义这个迭代器对象要返回一个 next() 方法，这个 next() 方法返回一个包含 value、done 属性的对象。
+ */
+class MyObject {
+    #i = 0;
+
+    constructor(items) {
+        this.items = items;
+    }
+
+    get length() {
+        return Object.keys(this.items).length;
+    }
+
+    // for...of 循环会用到
+    [Symbol.iterator]() {
+        return this;
+    }
+
+    // 迭代器接口方法
+    next() {
+        let done = this.#i >= this.length;
+        let value = !done ? Object.values(this.items)[this.#i++] : undefined;
+
+        return {
+            done: done,
+            value: value,
+        };
+    }
+}
+
+const obj = new MyObject({
+    a: 1,
+    b: 2,
+    c: 3,
+});
+
+// for (let item of obj) {
+//     console.log('item -> ', item);
+// }
+
+//
+// --------------------------------------------
+//
+
+/**
+ * 基于生成器函数的迭代器实现
+ * 使用生成器函数（Generator）实现是最简单的，只要使用 yield 语句返回每一次的值即可。
+ */
+const myIterable = {
+    items: Array.of(1, 2, 3, 4),
+
+    // 调用可迭代对象的 Symbol.iterator 方法会返回一个迭代器对象，它的接口中有一个 next 方法
+    // 了解生成器函数（Generator）的可能不会陌生，同样的当你执行一个生成器函数也会得到一个迭代器对象，但是要区分 生成器和迭代器不是一个概念。
+    *[Symbol.iterator]() {
+        yield* this.items;
+    },
+};
+
+for (let value of myIterable) {
+    console.log('[ value ]', value);
+}
+
+// 或者
+console.log('[ [...myIterable] ]', [...myIterable]);
